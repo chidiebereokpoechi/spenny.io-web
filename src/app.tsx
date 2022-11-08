eee6a70295e508f505110f9256c512df3e8421f6
Chids <chidiebere.okpoechi@gmail.com>
2022-11-08 15:28:50 +0000

    Add fonts

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const App: React.FC = () => {
     return (
         <div>
-            <span>Hello, world</span>
+            <span>4512.99</span>
+            <br />
+            <span>2340.99</span>
         </div>
     )
 }
